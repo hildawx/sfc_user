bbb3a7d8a6cf49c49d693739e59334a1d035887f
wuxin <wuxin@ambimmort.com>
2014-12-31 09:25:51 +0800

    修改登录验证，修改儿童cookie存储

--- src/main/webapp/pages/js/common.js
+++ src/main/webapp/pages/js/common.js
@@ -1,4 +1,9 @@
-
+// 获取地址栏参数值
+function GetQueryString(name){
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
 //读取Cookie的方法
 function getCookie(key) {
     var arr1 = document.cookie.split('; ');
@@ -16,6 +21,16 @@ function setCookie(key, value, t) {
     oDate.setDate(oDate.getDate() + t);
     document.cookie = key + '=' + encodeURI(value) + ';expire=' + oDate.toGMTString();
 }
+
+//删除cookies 
+function delCookie(name) 
+{ 
+    var exp = new Date(); 
+    exp.setTime(exp.getTime() - 1); 
+    var cval=getCookie(name); 
+    if(cval!=null) 
+    document.cookie= name + "="+cval+";expires="+exp.toGMTString(); 
+} 
 
 // 所有弹窗关闭将窗内的输入框置空
 $(".modal").on('hidden.bs.modal', function (e) {
